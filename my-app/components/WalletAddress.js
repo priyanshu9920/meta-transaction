0822a786287c1a005f659576c5756ba28e524a0e
priyanshu9920 <109070670+priyanshu9920@users.noreply.github.com>
2022-09-28 12:08:35 +0530

    Create WalletAddress.js

---
+++ my-app/components/WalletAddress.js
@@ -0,0 +1,8 @@
+import { Typography } from "@mui/material";
+import { useWeb3React } from "@web3-react/core";
+const WalletAddress = () => {
+    const {account} = useWeb3React();
+    return <Typography>{account}</Typography>
+};
+
+export default WalletAddress;
