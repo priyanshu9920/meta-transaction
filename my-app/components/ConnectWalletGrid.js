c655885804c5a16fdbc5fd72d23e1d352534a5db
priyanshu9920 <109070670+priyanshu9920@users.noreply.github.com>
2022-09-28 12:04:53 +0530

    Create ConnectWalletGrid.js

---
+++ my-app/components/ConnectWalletGrid.js
@@ -0,0 +1,16 @@
+import { Grid } from '@mui/material'
+import MetamaskButton from './MetamaskButton'
+import WalletAddress from './WalletAddress'
+
+const ConnectWalletGrid = () => {
+    return (<Grid container justifyContent="center" alignItems="center">
+    <Grid item xs={2}>
+      <MetamaskButton />
+    </Grid>
+    <Grid item xs={2}> 
+      <WalletAddress />       
+    </Grid>
+  </Grid>
+  );
+}
+export default ConnectWalletGrid;
